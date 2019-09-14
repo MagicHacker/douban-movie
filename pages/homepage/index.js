b1184e3e26f116aa429c37ab8d8da32b3e1be26d
Johnson <zxmagichacker@gmail.com>
2019-09-14 23:04:13 +0800

    feat: 添加catch处理

--- pages/homepage/index.js
+++ pages/homepage/index.js
@@ -5,37 +5,47 @@ Page({
    * 页面的初始数据
    */
   data: {
-    categoryList:['影院热映','即将上映','畅销图书','热门单曲榜'],
+    categoryList: ['影院热映', '即将上映', '畅销图书', '热门单曲榜'],
     // 影院热映
     theaterHot: [],
     movieComing: [],
     books: [],
     popularMusic: [],
-    homepageList:[]
+    homepageList: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  },
+  onLoad: function(options) {},
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-    
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-    Promise.all([api.request('movie/in_theaters', 'GET', { start: 1, count: 10 }), api.request('movie/coming_soon', 'GET', { start: 1, count: 10 }), api.request('book/search', 'GET', { q: 'Python', count: 10 }), api.request('music/search', 'GET', { q: '欧美', count: 10 })]).then(data => {
+  onShow: function() {
+    Promise.all([api.request('movie/in_theaters', 'GET', {
+      start: 1,
+      count: 10
+    }), api.request('movie/coming_soon', 'GET', {
+      start: 1,
+      count: 10
+    }), api.request('book/search', 'GET', {
+      q: 'Python',
+      count: 10
+    }), api.request('music/search', 'GET', {
+      q: '欧美',
+      count: 10
+    })]).then(data => {
       this.setData({
-        homepageList:[
-          {
-            title:'影院热映',
+        homepageList: [{
+            title: '影院热映',
             itemList: data[0].subjects
           },
           {
@@ -52,53 +62,55 @@ Page({
           }
         ]
       })
+    }).catch(err => {
+      console.log(err)
     })
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-    
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-    
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-    
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-    
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-    
+  onShareAppMessage: function() {
+
   },
   /**
    * 点击搜索
    */
-  searchMovie: function () {
+  searchMovie: function() {
     wx.navigateTo({
       url: '/pages/search/index',
     })
   },
   // 查看更多
-  seeMore:function (event) {
+  seeMore: function(event) {
     wx.navigateTo({
       url: '/pages/movie-more/index',
     })
